f0bb622d438848003178142148dd202e1e5cecbc
Magnus Åberg <himself@magnusaberg.me>
2024-02-10 14:59:00 +0100

    Added threading support, improved argument handling and user guidance
    - Incorporated multithreading to process multiple directories concurrently for efficient image resizing.
    - Added robust argument handling to check presence of mandatory CLI arguments, providing usage information to the user when necessary arguments are missing.
    - Refined rename_image logic to skip processing for files that already have resize dimensions in their filenames.

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,66 @@
+mod image_processing;
+
 #[deny(unsafe_code)]
 use std::env;
 use std::path::PathBuf;
 use std::process::exit;
 use glob::glob;
 use image::GenericImageView;
-use image::imageops::FilterType;
-use regex::Regex;
+use crate::image_processing::process_image;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 4 {
+        println!("USAGE: imhdlr <directory_with_images/**/*> <resize_width> <resize_height>");
+        return;
+    }
     let directory_with_images = &args[1];
     let resize_width = &args[2];
     let resize_height = &args[3];
-    let images = glob_images(directory_with_images);
-    for image in images {
-        process_image(
-            image,
-            resize_width
-                .parse()
-                .expect("Not a valid number for resize width"),
-            resize_height
-                .parse()
-                .expect("Not a valid number for resize height")
-        );
+
+    let directories = glob_images(directory_with_images);
+
+    let mut thread_handles = vec![];
+
+    for images in directories {
+        let thread_resize_width = resize_width
+            .parse()
+            .expect("Not a valid number for resize width");
+        let thread_resize_height = resize_height
+            .parse()
+            .expect("Not a valid number for resize height");
+        let handle = std::thread::spawn(move || {
+            for image in images {
+                process_image(image, thread_resize_width, thread_resize_height);
+            }
+        });
+        thread_handles.push(handle);
     }
 
-    exit(0)
+    for handle in thread_handles {
+        handle.join().unwrap();
+    }
+
+    exit(0);
 }
 
-fn glob_images(dir: &String) -> Vec<PathBuf>{
-    let mut images = vec![];
+fn glob_images(dir: &String) -> Vec<Vec<PathBuf>> {
+    let mut directories = vec![];
     let file_extensions = vec![".jpg", ".jpeg", ".gif", ".png"];
 
     for file_extension in file_extensions {
         let pattern = format!("{}{}", dir, file_extension);
+        let mut images = vec![];
         for entry in glob(&pattern).expect("Failed to read directories") {
             match entry {
                 Ok(path) => images.push(path),
                 Err(e) => println!("{:?}", e),
             }
         }
-    }
-
-    return images
-}
-
-fn process_image(path: PathBuf, resize_width: u32, resize_height: u32) {
-    let img = image::open(path.clone()).unwrap();
-    let file_path = path.into_os_string().into_string().unwrap();
-    let (width, height) = img.dimensions();
-
-
-    if resize_width <= width && resize_height <= height {
-        let resized = img.resize(resize_width, resize_height, FilterType::Triangle);
-        let renamed = rename_image(file_path, resize_width, resize_height);
-        match renamed {
-            Some(path) => {
-                dbg!(path.clone());
-                resized.save(path).unwrap();
-            }
-            None => (),
+        if !images.is_empty() {
+            directories.push(images);
         }
     }
-}
 
-fn rename_image(file_path: String, resize_width: u32, resize_height: u32) -> Option<String> {
-    let path_split: Vec<&str> = file_path.rsplitn(2, '/').collect();
-    let file_name_split: Vec<&str> = path_split[0].rsplitn(2, '.').collect();
-
-    let re = Regex::new("-\\d+x\\d+").unwrap();
-    if re.is_match(file_name_split[1]) {
-        println!("File {} already contains resize dimensions, skipping.", file_path);
-        None
-    } else {
-        let new_filename = format!("{}-{}x{}.{}", file_name_split[1], resize_width, resize_height, file_name_split[0]);
-        let new_path = format!("{}/{}", path_split[1], new_filename);
-
-        Some(new_path)
-    }
+    directories
 }
