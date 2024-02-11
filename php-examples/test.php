f8df74dc3fd8f14ea73226ae2d6bec4652f672e1
Magnus Åberg <himself@magnusaberg.me>
2024-02-11 05:39:42 +0100

    Refactor image handler functions and add cropping feature
    This update modifies the existing image handler functions, including renaming 'imhdlr_get_images' to 'imhdlr_get' and 'imhdlr_resize_images' to 'imhdlr_squeeze'. 'imhdlr_squeeze' no longer skips names with dimensions. Also introduced is a new image cropping function 'imhdlr_crop' to the image handler. These changes also affected tests and minor script alterations in 'test.php'. A new dependency 'fast_image_resize' was added to the project.

--- php-examples/test.php
+++ php-examples/test.php
@@ -1,11 +1,5 @@
 <?php
 
-var_dump("files counted",count(imhdlr_get_images('tests/images')));
-var_dump("skipnames: true", "verbose: false");
-imhdlr_resize_images('tests/images', 750, 750, true, false);
-var_dump("skipnames: false", "verbose: false");
-imhdlr_resize_images('tests/images', 600, 350, false, false);
-var_dump("skipnames: false", "verbose: false");
-imhdlr_resize_images('tests/images', 500, 500, false, false);
-var_dump("skipnames: true", "verbose: true");
-imhdlr_resize_images('tests/images', 400, 700, true, true);
+var_dump(count(imhdlr_get('tests/images')));
+var_dump(imhdlr_crop('tests/images', 600, 350));
+// imhdlr_squeeze('tests/images', 750, 750, true);
--- src/imhdlr/utils.rs
+++ src/imhdlr/utils.rs
@@ -1,10 +1,15 @@
+use ext_php_rs::props::Prop;
+use fast_image_resize as fr;
+use fast_image_resize::{DifferentTypesOfPixelsError, DynamicImageView, Image};
 use glob::glob;
-use image::imageops::FilterType;
-use image::GenericImageView;
+use image::io::Reader as ImageReader;
+use image::{imageops, GenericImageView, ImageBuffer, ImageResult, Rgba};
 use regex::Regex;
-use std::fs::create_dir_all;
+use std::fs::{create_dir_all, rename};
 use std::io;
+use std::num::NonZeroU32;
 use std::path::PathBuf;
+use std::process::exit;
 
 const FILE_EXTENSIONS: [&str; 10] = [
     ".jpg", ".jpeg", ".gif", ".png", ".avif", ".bmp", ".ico", ".tga", ".tiff", ".webp",
@@ -47,79 +52,60 @@ pub fn glob_images(dir: String) -> Vec<Vec<PathBuf>> {
     directories
 }
 
-pub fn process_image(
+pub fn process_squeeze(
     path: PathBuf,
     resize_width: u32,
     resize_height: u32,
-    skip_names_with_dimensions: bool,
     verbose: bool,
 ) -> io::Result<()> {
-    let img = match image::open(&path) {
-        Ok(img) => img,
-        Err(e) => {
-            eprintln!("Failed to open image {}: {}", path.display(), e);
-            return Err(io::Error::new(io::ErrorKind::Other, "Failed to open image"));
-        }
-    };
-
-    let file_path = match path.into_os_string().into_string() {
-        Ok(s) => s,
-        Err(_) => {
-            eprintln!("Failed to read the image path");
-            return Err(io::Error::new(
-                io::ErrorKind::Other,
-                "Failed to read the image path",
+    if let Ok(image) = ImageReader::open(&path) {
+        if let Ok(image) = image.decode() {
+            let resized_image = image.resize_exact(
+                resize_width,
+                resize_height,
+                image::imageops::FilterType::Lanczos3,
+            );
+            let output_path = path.with_file_name(rename_image(
+                path.file_name().unwrap().to_str().unwrap(),
+                resize_width,
+                resize_height,
             ));
-        }
-    };
-
-    let (width, height) = img.dimensions();
-
-    if resize_width <= width && resize_height <= height {
-        let resized = img.resize(resize_width, resize_height, FilterType::Triangle);
-        let mut validated_file_path = Some(file_path.clone());
-        if skip_names_with_dimensions {
-            validated_file_path = rename_image(file_path, resize_width, resize_height, verbose);
-        }
-
-        if let Some(path) = validated_file_path {
-            if let Err(e) = resized.save(&path) {
-                eprintln!("Failed to save resized image {}: {}", path, e);
-                return Err(io::Error::new(
-                    io::ErrorKind::Other,
-                    "Failed to save resized image",
-                ));
-            };
+            match resized_image.save(output_path.clone()) {
+                Ok(()) => {
+                    if verbose {
+                        println!("{} saved", output_path.display());
+                    }
+                }
+                Err(e) => {
+                    eprintln!("An error occurred when saving. {}", e);
+                }
+            }
         }
     }
-
     Ok(())
 }
 
-fn rename_image(
-    file_path: String,
-    resize_width: u32,
-    resize_height: u32,
-    verbose: bool,
-) -> Option<String> {
+pub fn process_crop(path: PathBuf, resize_width: u32, resize_height: u32) {
+    if let Ok(reader) = ImageReader::open(&path) {
+        if let Ok(mut image) = reader.decode() {
+            let subimg = imageops::crop(&mut image, 0, 0, resize_width, resize_height);
+            let output_path = path.with_file_name(rename_image(
+                path.file_name().unwrap().to_str().unwrap(),
+                resize_width,
+                resize_height,
+            ));
+            subimg.to_image().save(output_path).unwrap();
+        }
+    }
+}
+
+fn rename_image(file_path: &str, resize_width: u32, resize_height: u32) -> String {
     let path_split: Vec<&str> = file_path.rsplitn(2, '/').collect();
     let file_name_split: Vec<&str> = path_split[0].rsplitn(2, '.').collect();
-    let re = Regex::new("-\\d+x\\d+").unwrap();
-    if re.is_match(file_name_split[1]) {
-        if verbose {
-            println!(
-                "File {} already contains resize dimensions, skipping.",
-                file_path
-            );
-        }
-        None
-    } else {
-        let new_filename = format!(
-            "{}-{}x{}.{}",
-            file_name_split[1], resize_width, resize_height, file_name_split[0]
-        );
-        let new_path = format!("{}/{}", path_split[1], new_filename);
+    let new_filename = format!(
+        "{}-{}x{}.{}",
+        file_name_split[1], resize_width, resize_height, file_name_split[0]
+    );
 
-        Some(new_path)
-    }
+    new_filename
 }
