67ffac9c8d8efbf4ba392b805083843ca36049a5
Magnus Åberg <himself@magnusaberg.me>
2024-02-19 19:46:03 +0100

    Add php examples

--- php-examples/test.php
+++ php-examples/test.php
@@ -1,5 +1,6 @@
 <?php
 
 var_dump(count(imhdlr_get('tests/images')));
-var_dump(imhdlr_crop('tests/images', 600, 350));
-// imhdlr_squeeze('tests/images', 750, 750, true);
+var_dump(imhdlr_crop('tests/images', 600, 350, true));
+var_dump(imhdlr_squeeze('tests/images', 750, 750, true));
+var_dump(imhdlr_squeeze_and_crop('tests/images', 400, 400, true));
